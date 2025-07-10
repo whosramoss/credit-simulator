d9ef9d591dace4e46cc6d78ceb841d5be2040162
Gabriel Ramos <whosramoss@gmail.com>
2025-07-10 12:29:10 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      Welcome to <a href="https://nextjs.org">Next.js!</a>
+    </React.Fragment>
+  );
+}
