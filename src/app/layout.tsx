ee11a773b10fce069c9cd6321739d50bd77611e0
Gabriel Ramos <whosramoss@gmail.com>
2025-07-10 12:37:37 -0300

    chore: update font and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,22 @@
 import { ReactNode } from "react";
+import type { Metadata } from "next";
+import { Questrial } from "next/font/google";
 
+const fontQuestrial = Questrial({
+    subsets: ["latin"],
+    display: "block",
+    weight: ["400"],
+});
+
+export const metadata: Metadata = {
+    title: "Simulador de Empréstimo",
+    description: "Simule seu empréstimo de forma rápida e transparente",
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-       {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className={fontQuestrial.className}>{children}</body>
+        </html>
+    );
 }
