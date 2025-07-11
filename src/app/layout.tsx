5e27b92cd8564fe4457ddeb668acf7aa64c13b0c
Gabriel Ramos <whosramoss@gmail.com>
2025-07-11 02:29:29 -0300

    chore: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,17 +9,89 @@ const fontQuestrial = Questrial({
     weight: ["400"],
 });
 
-export const metadata: Metadata = {
-    title: "Simulador de Empréstimo",
-    description: "Simule seu empréstimo de forma rápida e transparente",
-};
-
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
-        <html lang="en" suppressHydrationWarning>
+        <html lang="pt" suppressHydrationWarning>
             <body className={fontQuestrial.className}>
                 <StyledComponentsTheme>{children}</StyledComponentsTheme>
             </body>
         </html>
     );
 }
+
+export const metadata: Metadata = {
+    title: "Simulador de Empréstimo",
+    description: "Simule seu empréstimo de forma rápida e transparente",
+    keywords: [
+        "empréstimo",
+        "simulador",
+        "crédito",
+        "financeiro",
+        "calculadora",
+        "online",
+        "pessoal",
+        "rápido",
+    ],
+    openGraph: {
+        type: "website",
+        siteName: "Simulador de Empréstimo",
+        locale: "pt_BR",
+        url: "https://credit-simulator.vercel.app",
+        title: "Simulador de Empréstimo",
+        description: "Simule seu empréstimo de forma rápida e transparente",
+        images: [
+            {
+                url: "/thumbnail.png",
+                width: 1200,
+                height: 630,
+                alt: "Simulador de Empréstimo Preview",
+            },
+        ],
+    },
+    authors: [
+        {
+            name: "Gabriel Ramos",
+            url: "https://whosramoss.com",
+        },
+    ],
+    creator: "Gabriel Ramos",
+    icons: [
+        {
+            rel: "icon",
+            url: "/favicon.ico",
+        },
+        {
+            rel: "apple-touch-icon",
+            url: "/apple-touch-icon.png",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            url: "/favicon-32x32.png",
+            sizes: "32x32",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            url: "/favicon-16x16.png",
+            sizes: "16x16",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            url: "/android-chrome-192x192.png",
+            sizes: "192x192",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            url: "/android-chrome-512x512.png",
+            sizes: "512x512",
+        },
+    ],
+    robots: {
+        index: true,
+        follow: true,
+    },
+    manifest: "/site.webmanifest",
+};
